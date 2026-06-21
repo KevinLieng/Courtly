715f24a7e423079cbfd3cd686c0c1177344691fe
Kevin Lieng <kevinlieng1@gmail.com>
2026-06-22 00:29:28 +1000

    changed icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import CourtTable from "./components/courtTable";
 
 export default function App() {
   return (
-    <>
+    <> 
       <h1>Courtly</h1>
-
+      <> Find nearby tennis courts available to book </>
       <CourtTable />
     </>
   )
